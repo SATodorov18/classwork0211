60dd1548e2d5f2c98207b319cb0351601bb4e462
SATodorov18 <SATodorov18@codingburgas.bg>
2022-11-02 14:32:46 +0200

    initial commit

---
+++ src/main/java/com/example/classwork0211/Classwork0211Application.java
@@ -0,0 +1,13 @@
+package com.example.classwork0211;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Classwork0211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Classwork0211Application.class, args);
+    }
+
+}
